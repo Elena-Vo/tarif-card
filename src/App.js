63e58ac82ec69ff39246ef90f3fe44fbed1e6ee4
ElVo <elvor.info@gmail.com>
2025-03-31 17:29:54 +0300

    add states for card and refactor after code review

--- src/App.js
+++ src/App.js
@@ -3,38 +3,64 @@ import './App.css';
 import Card from "./components/card"
 
 function App() {
+  const cards = [
+    {
+      id: 1,
+      title: "Безлимитный 300",
+      price: "300",
+      speed: "до 10 Мбит/сек",
+      traffic: "Объем включенного трафика не ограничен",
+      titleColor: "#229cab",
+      priceColor: "#2cc1d5"
+    },
+    {
+      id: 2,
+      title: "Безлимитный 450",
+      price: "450",
+      speed: "до 50 Мбит/сек",
+      traffic: "Объем включенного трафика не ограничен",
+      titleColor: "#2cc1d5",
+      priceColor: "#24ae8d"
+    },
+    {
+      id: 3,
+      title: "Безлимитный 550",
+      price: "550",
+      speed: "до 100 Мбит/сек",
+      traffic: "Объем включенного трафика не ограничен",
+      titleColor: "#c4554e",
+      priceColor: "#f46a5f"
+    },
+    {
+      id: 4,
+      title: "Безлимитный 1000",
+      price: "1000",
+      speed: "до 200 Мбит/сек",
+      traffic: "Объем включенного трафика не ограничен",
+      titleColor: "#282f37",
+      priceColor: "#333a44"
+    }
+  ];
+
+
+
   return (
     <div className="App">
       <div className="card-container">
+      {cards.map((card) => (
         <Card
-          title="Безлимитный 300"
-          price="300"
-          speed="до 10 Мбит/сек"
-          titleColor="#229cab" 
-          priceColor="#2cc1d5" 
+          key={card.id}
+          title={card.title}
+          price={card.price}
+          speed={card.speed}
+          titleColor={card.titleColor}
+          priceColor={card.priceColor}
+          traffic={card.traffic}
+      
         />
-        <Card
-          title="Безлимитный 450"
-          price="450"
-          speed="до 50 Мбит/сек"
-          titleColor="#2cc1d5" 
-          priceColor="#24ae8d" 
-        />
-        <Card
-          title="Безлимитный 550"
-          price="550"
-          speed="до 100 Мбит/сек"
-          isTall
-          titleColor="#c4554e" 
-          priceColor="#f46a5f" 
-        />
-        <Card
-          title="Безлимитный 1000"
-          price="1000"
-          speed="до 200 Мбит/сек"
-          titleColor="#282f37" 
-          priceColor="#333a44" 
-        />
+      )) 
+      }
+  
       </div>
     </div>
   );
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,17 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from "prop-types";
 import "./card.css";
 
-const Card = ({ title, price, speed, isTall, titleColor, priceColor }) => {
+const Card = ({ title, price, speed, titleColor, priceColor, traffic }) => {
+  const [isTall, setIsTall] = useState(false);
+  const toggleTall = () => {
+    setIsTall(!isTall)
+  };
+
+
   return (
-    <div className={`card ${isTall ? 'tall' : ''}`}>
+    <div className={`card ${isTall ? 'tall' : ''}`} onClick={toggleTall}>
       <div className="section title" style={{ backgroundColor: titleColor }}>{title}</div>
       <div className="section price" style={{ backgroundColor: priceColor }}>
       <div className="price-stairs">
@@ -14,9 +21,23 @@ const Card = ({ title, price, speed, isTall, titleColor, priceColor }) => {
         </div>
       </div>
       <div className="section speed">{speed}</div>
-      <div className="section traffic">Объем включенного трафика не ограничен</div>
+      <div className="section traffic">{traffic}</div>
     </div>
   );
 };
+Card.propTypes = {
+  title: PropTypes.string.isRequired, // Строка, обязательно
+  price: PropTypes.number.isRequired, // Число, обязательно
+  speed: PropTypes.string.isRequired, // Строка, обязательно
+  titleColor: PropTypes.string, // Строка, не обязательно
+  priceColor: PropTypes.string, // Строка, не обязательно
+  traffic: PropTypes.string // Строка, не обязательно
+};
+
+Card.defaultProps = {
+  titleColor: "#fff",
+  priceColor: "#fff",
+  traffic: "Объем включенного трафика не ограничен"
+}
 
 export default Card;
